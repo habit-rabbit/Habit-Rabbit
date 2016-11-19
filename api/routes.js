e0bbd54d234f923bd54b3b6d01a01ebe11382b77
Patrick Simonian <patricksimonian@gmail.com>
2016-11-19 00:36:20 -0800

    seperated routing and tested working

--- api/routes.js
+++ api/routes.js
@@ -1,35 +1,39 @@
 const router = require('express').Router();
 const db = require("./query_class.js");
+const usersRoutes = require("./users_routes.js")
 //routes that serve the data base and return json
 
+router.use("/api", usersRoutes);
+router.use("/api", goalsRoutes);
 
-router.post("/api/users", (req, res) => {
-  let query = req.body;
-  req.body.table = "users"; //for definition required by db (need to dry up)
-  //this route implies we are looking to insert into users table
-  db.insert(query, callback)
-});
+// router.post("/api/users", (req, res) => {
+//   //this route implies we are looking to insert into users table
+//   let query = req.body;
+//   query.table = "users"; //for definition required by db (need to dry up)
+//   db.insert(query, callback)
+// });
 
-router.get("/api/users", (req, res) => {
-  req.query.table = "users";
-  let query = req.query;
-  db.getAll(query, callback)
-})
+// router.get("/api/users", (req, res) => {
+//   let query = req.query;
+//   query.table = "users";
+//   db.getAll(query, callback)
+// })
 
-router.get("/api/users/:id", (req, res) => {
-  req.query.table = "users";
-  req.query.data = {};
-  req.query.data.id = req.params.id;
-  // we can assign the req.params.id to our query
-  // since we are getting only one user
-  let query = req.query;
-  db.getRow(query, callback)
-})
+// router.get("/api/users/:id", (req, res) => {
+//   let query = req.query;
+//   query.table = "users";
+//   // we can assign the req.params.id to our data object, but as there is no
+//   // data object being passed in from the ajax call we create an empty one
+//   //this preserves the formatting required for the database class
+//   query.data = {};
+//   query.data.id = req.params.id;
+//   db.getRow(query, callback)
+// })
 
 
-function callback(data) {
-  console.log(data);
-  console.log("works");
-}
+// function callback(data) {
+//   console.log(data);
+//   console.log("works");
+// }
 
 module.exports = router;
