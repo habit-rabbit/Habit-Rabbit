c84c5c3bf33146931b9fbd19cbefbca8cb5d46a8
Patrick Simonian <patricksimonian@gmail.com>
2016-11-19 12:40:28 -0800

    user_routes controller for /api/users now send array back to client, no error handling imposed yet

--- api/users_routes.js
+++ api/users_routes.js
@@ -8,6 +8,7 @@ router.post("/users", (req, res) => {
   let query = req.body;
   query.table = "users"; //for definition required by db (need to dry up)
   db.insert(query, callback);
+
 });
 
 router.get("/users", (req, res) => {
@@ -16,35 +17,54 @@ router.get("/users", (req, res) => {
   if(req.xhr) {
     let query = req.query;
     query.table = "users";
-    db.getAll(query, callback);
+    db.getAll(query, (data) => {
+      console.log("success");
+      //sends an array back
+      res.send(data)
+    });
   } else {
     res.redirect("/");
   }
 })
 
 router.get("/users/:id", (req, res) => {
-  let query = req.query;
-  query.table = "users";
-  // we can assign the req.params.id to our data object, but as there is no
-  // data object being passed in from the ajax call we create an empty one
-  //this preserves the formatting required for the database class
-  query.data = {};
-  query.data.id = req.params.id;
-  db.getRow(query, callback);
+
+  if(req.xhr) {
+    let query = req.query;
+    query.table = "users";
+    // we can assign the req.params.id to our data object, but as there is no
+    // data object being passed in from the ajax call we create an empty one
+    //this preserves the formatting required for the database class
+    query.data = {};
+    query.data.id = req.params.id;
+    db.getRow(query, callback, res);
+  } else {
+    res.redirect("/")
+  }
 })
 
 //for the sake of demo and lack of willingness to set up method override
 // delete and update methods will be posts for now..
 
 router.post("/users/:id/update", (req, res) => {
-  let query = req.body
-  query.table = "users";
-  query.data.id = req.params.id;
-  db.updateRow(query, callback);
+  if(req.xhr) {
+    let query = req.body
+    query.table = "users";
+    query.data.id = req.params.id;
+    db.updateRow(query, callback);
+  } else {
+    res.redirect("/")
+  }
 })
 module.exports = router;
 
-function callback(data) {
-  console.log(data);
-  console.log("works");
-}
+// function callback(data) {
+//   console.log(data);
+//   console.log("works");
+//   res.JSON("Good");
+// }
+
+// function response(res) {
+
+//   res.send("okay");
+// }
