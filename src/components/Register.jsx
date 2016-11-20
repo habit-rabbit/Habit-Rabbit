d6de41023ddc4661bc846c712f5f3ad6f17f64e2
cecstar <yocecia@gmail.com>
2016-11-20 02:50:33 +0000

    refactored Register form with bootstrap. Refactored Login form with bootstrap. Added stlying for each to make them uniform and more user friendly. **SCSS needs to be restyled so 'input' does not affect the Button. Currently button width is too wide, however for now it's acceptable. Both forms are consistent and can be retouched when we have time (or better button-editing skills).

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,22 +13,31 @@ class Register extends Component {
 
             <div className="modal-header">
               <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h1 className="modal-title" id="myModalLabel">Register For Your Account</h1>
+                <h1 className="modal-title" id="myModalLabel">Sign Up For Your Account</h1>
             </div>
 
-            <div className="modal-body">
-              <form>
-
-                  <input type="text" name="first_name" placeholder="First Name"/>
-                  <input type="text" name="last_name" placeholder="Last Name"/>
-                  <input type="text" name="email" placeholder="Email"/>
-                  <input type="password" name="password" placeholder="Password"/>
-                  <input type="password" name="password_confirmation" placeholder="Password Confirmation"/>
-                  <input type="submit" name="register" value="Register" data-dismiss="modal"/>
-
-              </form>
+            <div id="register-body">
+            <form className="form-horizontal">
+              <div className="form-group">
+                <input type="text" name="first_name" placeholder="First Name"/>
+              </div>
+              <div className="form-group">
+                <input type="text" name="last_name" placeholder="Last Name"/>
+              </div>
+              <div className="form-group">
+                <input type="text" name="email" placeholder="Email"/>
+              </div>
+              <div className="form-group">
+                <input type="password" name="password" placeholder="Password"/>
+              </div>
+              <div className="form-group">
+                <input type="password" name="password_confirmation" placeholder="Password Confirmation"/>
+              </div>
+              <div className="form-group">
+                <input type="submit" name="register" className="btn btn-default" value="Sign On Up!" data-dismiss="modal"/>
+              </div>
+            </form>
             </div>
-
           </div>
         </div>
       </div>
