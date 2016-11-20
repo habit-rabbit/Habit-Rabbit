e7ebfa66d2754fd502fd7448beef2ddc6ffadd67
Nat York <natyork@gmail.com>
2016-11-20 00:48:07 +0000

    update styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,52 +1,55 @@
 import React, {Component} from 'react';
-
+import Nav from './Nav.jsx';
 
 class Login extends Component {
 
+  // constructor(props) {
+  //   super(props);
+  //   this.users = {};
+  // }
 
-  constructor(props) {
-    super(props);
-    this.users = {};
-  }
-
-  componentWillMount() {
-    // for demonstration
-    // $.ajax({
-    //   method: 'get',
-    //   url: '/api/goals/3',
-      // data: {
-      //   data: {
-      //     first_name: "UltraaSerious",
-      //     last_name: "Granger",
-      //     email: "iLoveCats@hogwarts.uk",
-      //     password_digest: "notdigested"
-      //   }
-      // }
-    // });
-  }
+  // componentWillMount() {
+  //   for demonstration
+  //   $.ajax({
+  //     method: 'get',
+  //     url: '/api/goals/3',
+  //     data: {
+  //       data: {
+  //         first_name: "UltraaSerious",
+  //         last_name: "Granger",
+  //         email: "iLoveCats@hogwarts.uk",
+  //         password_digest: "notdigested"
+  //       }
+  //     }
+  //   });
+  // }
 
   render() {
     console.log("Rendering <Login/>");
 
     return (
+      <div className="modal fade" id="login-modal" tabIndex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
 
-      <div className="jumbotron">
-        <h1>Hello, world!</h1>
-        <p>...</p>
-  <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-  <div className="login">
-          <h1>Login to Your Account</h1><br/>
+            <div className="modal-header">
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h1 className="modal-title" id="myModalLabel">Login To Your Account</h1>
+            </div>
 
-          <form>
-            <input type="text" name="email" placeholder="Email"/>
-            <input type="password" name="password" placeholder="Password"/>
-            <input type="submit" name="login" value="Login"/>
-          </form>
+            <div className="modal-body">
+              <form>
+                <input type="text" name="email" placeholder="Email"/>
+                <input type="password" name="password" placeholder="Password"/>
+                <input type="submit" name="login" value="Login" data-dismiss="modal"/>
+              </form>
+            </div>
 
+          </div>
+        </div>
       </div>
-</div>
-
     );
   }
 }
+
 export default Login
