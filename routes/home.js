3e1f6f33377b980ba49a3b069220ec790b30188b
Patrick Simonian <patricksimonian@gmail.com>
2016-11-19 19:03:51 -0800

    complete bcyrpt hash compare function

--- routes/home.js
+++ routes/home.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const db = require("../api/query_class.js");
+const bcrypt = require('bcrypt');
 router.get('/', (req, res) => {
   res.render('index');
 });
 
 
 router.post('/login', (req, res) => {
+  const pw = req.body.data.password;
   const query = {};
   query.data = {};
   query.table = 'users';
@@ -14,9 +16,13 @@ router.post('/login', (req, res) => {
   /// password entered in req.body.data.password
   db.getRow(query, (err, data) => {
     let user = data[0];
+    // compare password to has...
+    bcrypt.compare(pw , user.password_digest, function(err, result) {
+      result ? console.log("works") : console.log("not worked");
 
+    // res == true
+  });
   })
-  console.log(query);
 
 })
 module.exports = router;
