9769bdd3460ec254769d0161306d419a81d6705a
Patrick Simonian <patricksimonian@gmail.com>
2016-11-19 19:36:02 -0800

    integrated the Response Class with /login

--- routes/home.js
+++ routes/home.js
@@ -14,40 +14,29 @@ router.post('/login', (req, res) => {
   query.data = {};
   query.table = 'users';
   query.data.email = req.body.data.email;
-  //query data base for user, if found grab hash and compare to
-  /// password entered in req.body.data.password
+  //query data base for user, if found grab hash and compare to pw
   db.getRow(query, (err, data) => {
-    let user = data[0];
-    // compare password to has...
-    bcrypt.compare(pw , user.password_digest, function(err, result) {
-      result ? console.log("works") : console.log("not worked");
+    //if err, the query was made with an invalid email;
+    if (err) {
+      r.setErrorMsg("Invalid credentials, please try again or signup")
+      res.send(r);
+    }
+    if (!r.getData()) {
+      //if data is empty that means the email supplied did not match
+      // a record in the db
+      r.setErrorMsg(`Incorrect email or password!`);
+      res.send(r);
+    } else {
+      //database found user
+      let user = data[0];
+      // compare password to has...
+      bcrypt.compare(pw , user.password_digest, function(err, result) {
 
-    // res == true
-  });
+
+      // res == true
+      });
+    }
   })
 
 })
 module.exports = router;
-    // let query = req.query;
-    // query.table = "users";
-    // // we can assign the req.params.id to our data object, but as there is no
-    // // data object being passed in from the ajax call we create an empty one
-    // //this preserves the formatting required for the database class
-    // query.data = {};
-    // query.data.id = req.params.id;
-    // db.getRow(query, (err, data) => {
-    //   //if there is an error that means a query was made with an invalid id
-    //   // eg id = 'abcd'
-    //   if (err) {
-    //     r.setErrorMsg("Queried with invalid id!")
-    //   }
-    //   if (!r.getData()) {
-    //     //if data is empty that means the id that was supplied for the query
-    //     //does not exist in the database for users table
-    //     r.setErrorMsg(`User does not exist!`);
-    //   }
-    //   r.setData(data);
-    //   console.log("success");
-    //   //sends an array back
-    //   res.send(r);
-    // });
