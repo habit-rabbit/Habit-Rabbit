d9493ea18ca85440c0bb61dab0f7dab7a903b6bd
Nat York <natyork@gmail.com>
2016-11-21 19:41:52 +0000

    update knexfile for prod

--- knexfile.js
+++ knexfile.js
@@ -40,9 +40,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
