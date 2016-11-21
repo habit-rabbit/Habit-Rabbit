15bfd40a62e925b297cdf1d0df3b2fce0a6dcade
Patrick Simonian <patricksimonian@gmail.com>
2016-11-20 18:05:39 -0800

    more validation methods added to validations class

--- api/utilities/validations.js
+++ api/utilities/validations.js
@@ -1,32 +1,47 @@
+const db = require('../query_class.js');
+
 function Validations(data) {
   this.tests = [];
   this.data = data;
-  // methods
-
-  this.isNotEmpty = function() {
-    let string = this.data.trim();
-    return string.length > 0;
-  }
   this.isNumber = function() {
     return Number.isInteger(this.data);
   }
 }
-var v = new Validations();
 
-Validations.prototype.validatesEmpty = function(string) {
-  this.tests.push(this.isNotEmpty(string));
+Validations.prototype.validatesEmpty = function() {
+  let string = this.data.trim();
+  this.tests.push(string.length > 0);
   return this;
 }
 
 Validations.prototype.validatesNumber = function() {
-  this.tests.push(this.isNumber);
+  this.tests.push(Number.isInteger(this.data));
+  return this;
 }
+Validations.prototype.validatesEmail = function() {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const email = this.data.toLowerCase();
+  this.tests.push(re.test(email));
+  return this;
+}
+Validations.prototype.validatesUnique = function(column, table) {
+  const query = {};
+  const query.table = table;
 
-Validations.prototype.check = function() {
-  this.tests.forEach((element) => {
-    if(element() === false) return false;
+  db.getAll(query, (err, data) => {
+    data.forEach((elm) => {
+      console.log(elm, "yay");
+    })
   });
-  return true;
 }
+Validations.prototype.check = function() {
+  let result = this.tests.every(function(elm) {
+    return elm == true;
+  })
+  return result;
+}
+var v = new Validations(13);
+var t = new Validations("");
+console.log(v.validatesNumber().check());
+console.log(t.validatesNumber().validatesEmpty().check());
 
-console.log(v.isNotEmpty(""))
