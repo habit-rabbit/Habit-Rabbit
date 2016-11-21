6e64296de3edc7f284375fc7c000a22933e1d629
Patrick Simonian <patricksimonian@gmail.com>
2016-11-20 17:29:28 -0800

    testing ability to chain methods

--- api/utilities/validations.js
+++ api/utilities/validations.js
@@ -1,8 +1,32 @@
-function Validations() {
+function Validations(data) {
   this.tests = [];
+  this.data = data;
+  // methods
 
-  function isNotEmpty(string) {
-    string = string.trim();
-    string.length > 0 ? return true: return false;
+  this.isNotEmpty = function() {
+    let string = this.data.trim();
+    return string.length > 0;
+  }
+  this.isNumber = function() {
+    return Number.isInteger(this.data);
   }
 }
+var v = new Validations();
+
+Validations.prototype.validatesEmpty = function(string) {
+  this.tests.push(this.isNotEmpty(string));
+  return this;
+}
+
+Validations.prototype.validatesNumber = function() {
+  this.tests.push(this.isNumber);
+}
+
+Validations.prototype.check = function() {
+  this.tests.forEach((element) => {
+    if(element() === false) return false;
+  });
+  return true;
+}
+
+console.log(v.isNotEmpty(""))
