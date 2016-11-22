0799d77e0e1486281611e8daf3f951c3b27fa8b3
Patrick Simonian <patricksimonian@gmail.com>
2016-11-22 15:43:32 -0800

    add renderfunction for tasks so they are formatted as a bootstrap table for now

---
+++ src/components/Goal-info.jsx
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+
+class GoalInfo extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.goalType = this.goalType.bind(this);
+    this.renderTaskTable = this.renderTaskTable.bind(this);
+  }
+  renderTaskTable(task) {
+    return(<tr>
+              <th scope="row">sdf/th>
+              <td>sdf</td>
+            </tr>);
+
+  }
+  goalType (goal) {
+    if(goal.private === false){
+      return (
+        <h4 className="goal-type"> Challenge </h4>
+      )
+    } else {
+      return (
+        <h4 className="goal-type"> Private </h4>
+      )
+    }
+  }
+
+
+
+  render() {
+    return (
+          <div className="col-md-3">
+            <h1> {this.props.goalInfo.name} </h1>
+            {this.goalType(this.props.goalInfo)}
+            <table className="table table-sm table-inverse">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Task</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.props.tasks.map( (task) => {
+                  console.log(task, "task?")
+                  return this.renderTaskTable(task);
+                })}
+              </tbody>
+              </table>
+          </div>
+          );
+  }
+
+}
+  export default GoalInfo;
