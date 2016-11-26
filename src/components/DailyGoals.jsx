e5c85f5247cb4cc18277170299dd2b1c98cb0b17
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-26 21:46:02 +0000

    show name of goal, editing icons, create new goal button

--- src/components/DailyGoals.jsx
+++ src/components/DailyGoals.jsx
@@ -10,6 +10,8 @@ class DailyGoals extends Component {
       dailies: [],
     }
     this.renderDailyGoals = this.renderDailyGoals.bind(this);
+    // this.handleCheck = this.handleCheck.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount () {
@@ -21,9 +23,15 @@ class DailyGoals extends Component {
     });
   }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    // $("#create-daily-goal-modal").modal();
+    console.log("You clicked me! At least buy me dinner first! RUDE");
+  }
+
   renderDailyGoals () {
     console.log("In renderDailyGoals===================================================")
-    if(!this.state.dailes) {
+    if(!this.state.dailies) {
       console.log("In the if statement of renderDailyGoals====================================================");
       return (
         <div>
@@ -31,6 +39,31 @@ class DailyGoals extends Component {
           <h3> You should probably make one. </h3>
           <h3> Like brush your teeth? Maybe? I don't know your life. </h3>
           <h3> Figure it out. </h3>
+        </div>
+      );
+    }
+    else {
+      return (
+        <div>
+          {this.state.dailies.map((dailyGoal, index) => {
+            return (
+              <div className="daily-goals-template row well" key={index}>
+                <div className="col-md-7">
+                  <h1> {dailyGoal.name} </h1>
+                </div>
+                <div className="daily-goals-icons">
+                  <i className="fa fa-check-square" aria-hidden="true" onClick={this.handleCheck}></i>
+                  <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+                  <i className="fa fa-trash" aria-hidden="true"></i>
+                </div>
+              </div>
+            );
+          })}
+          <div>
+            <form className="daily-goals-form" onSubmit={this.handleSubmit}>
+              <input type="submit" className="btn btn-default" value="Create A New Daily Goal!" />
+            </form>
+          </div>
         </div>
       );
     }
