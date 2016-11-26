a644eb5ef33e6d2eb98fdbeb10bf7e6cba7f8d43
Courtney Reynoldson <ctreynoldson@gmail.com>
2016-11-26 21:03:12 +0000

    include font awesome, check if a user has no daily goals and render appropriate page view

--- src/components/DailyGoals.jsx
+++ src/components/DailyGoals.jsx
@@ -6,6 +6,34 @@ class DailyGoals extends Component {
 
   constructor(props){
     super(props);
+    this.state = {
+      dailies: [],
+    }
+    this.renderDailyGoals = this.renderDailyGoals.bind(this);
+  }
+
+  componentDidMount () {
+    $.ajax({
+      method: "get",
+      url: "/api/daily_goals",
+    }).done((response) => {
+      this.setState({dailies: response.data});
+    });
+  }
+
+  renderDailyGoals () {
+    console.log("In renderDailyGoals===================================================")
+    if(!this.state.dailes) {
+      console.log("In the if statement of renderDailyGoals====================================================");
+      return (
+        <div>
+          <h3> You don't have any daily goals yet! </h3>
+          <h3> You should probably make one. </h3>
+          <h3> Like brush your teeth? Maybe? I don't know your life. </h3>
+          <h3> Figure it out. </h3>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -14,6 +42,7 @@ class DailyGoals extends Component {
       <div className="background-layout">
         <div className="container main-content">
           <h1 className="container-title">Daily Goals</h1>
+          {this.renderDailyGoals()}
         </div>
       </div>
     );
