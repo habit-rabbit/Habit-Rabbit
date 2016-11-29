e71d1cbfeabc698cb6a3334a16429109a6b5ada6
Nat York <natyork@gmail.com>
2016-11-29 04:38:16 +0000

    Implement conditional logic for new badge alert

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,6 +10,7 @@ class Nav extends Component {
     this.logOut = this.logOut.bind(this);
     this.handleLinks = this.handleLinks.bind(this)
     this.handleKey = this.handleKey.bind(this);
+    this.newBadge = this.newBadge.bind(this);
   }
 
   componentWillMount() {
@@ -42,7 +43,7 @@ class Nav extends Component {
     if(this.props.isLoggedIn) {
       return (
         <ul className="nav navbar-nav navbar-right">
-          <li><a href="" id="new-badge" onClick={this.handleLinks}><span id="new-badge" onClick={this.handleLinks} className="glyphicon glyphicon-star" aria-hidden="true"></span></a></li>
+        {this.newBadge()}
           <li className="dropdown">
             <a href="" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Hey {this.props.name}!<span className="caret"></span></a>
             <ul className="dropdown-menu">
@@ -80,6 +81,19 @@ class Nav extends Component {
     }
     if (id === "badges" || id === "new-badge"){
       this.props.setView(3);
+      this.props.resetBadgeAlert();
+    }
+  }
+
+  newBadge(){
+    if (this.props.newBadge === true){
+      return (
+        <li>
+          <a href="" id="new-badge" onClick={this.handleLinks}>
+            <span id="new-badge" onClick={this.handleLinks} className="glyphicon glyphicon-star" aria-hidden="true"></span>
+          </a>
+        </li>
+      );
     }
   }
 
